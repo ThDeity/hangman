c42317e9ccc9f1eecdc203ac8515967f0f4ca35a
ThDeity <kapollonov@yandex.ru>
2022-07-19 11:57:09 +0300

    commit

---
+++ Xaydi/Assets/PlayerMovement.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public GameManager gm;
+
+    public float runSpeed = 500f;
+    public float strafeSpeed = 500f;
+    public float jumpForce = 15f;
+
+    protected bool strafeLeft = false;
+    protected bool strafeRight = false;
+    protected bool doJump = false;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Obstacle")
+        {
+            gm.EndGame();
+
+            Debug.Log("Конец игры!");
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKey("a"))
+        {
+            strafeLeft = true;
+        }
+        else
+        {
+            strafeLeft = false;
+        }
+
+        if (Input.GetKey("d"))
+        {
+            strafeRight = true;
+        }
+        else
+        {
+            strafeRight = false;
+        }
+
+        if (Input.GetKeyDown("space"))
+        {
+            doJump = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(transform.position + Vector3.forward * runSpeed * Time.deltaTime);
+
+        if (strafeLeft)
+        {
+            rb.AddForce(-strafeSpeed * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (strafeRight)
+        {
+            rb.AddForce(strafeSpeed * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (doJump)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+
+            doJump = false;
+        }
+    }
+}
